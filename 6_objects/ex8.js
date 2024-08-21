8957b5cfd53f268aab550928abd3aa9d0577af3c
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-08-21 10:54:55 +0530

    Update ex8.js

--- 6_objects/ex8.js
+++ 6_objects/ex8.js
@@ -7,12 +7,23 @@ const val1 = student.name;
 const val2 = student.roll;
 console.log(val1, " ", val2); //vikas   12
 //but we have shortcut to do this
+//------------------------------------------
 //we can do destructuring
 const { name, roll } = student;
 console.log(name); //vikas
 console.log(roll); //12
 //so here name and roll are 2 variable and its value will be assigned form the student object
 //but here we have to keep the name of the variable similar to key object
+
+let student={
+  name:"vikas",
+  roll:101,
+  age:23,
+  add:"rajpur",
+};
+const {v1,roll,age,add} =student;
+console.log(v1,age,roll,add);//undefined 23 101 rajpur
+//----------------------------------------------------
 //but if we want to change the variable name we can do like this
 const { name: nameval } = student;
 console.log(nameval); //vikas
