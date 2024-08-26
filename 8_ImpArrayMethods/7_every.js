c5c6e1fd0e81863720d9775944072f673562ec80
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-08-26 18:03:52 +0530

    Update 7_every.js

---
+++ 8_ImpArrayMethods/7_every.js
@@ -0,0 +1,32 @@
+/*
+every()
+=========
+The every() method in JavaScript is used to test whether all elements in an array pass the test implemented by the provided function. If all elements satisfy the condition, every() returns true. Otherwise, it returns false.
+
+Syntax
+--------
+array.every(callback(element, index, array), thisArg);
+
+Parameters
+---------
+callback: A function that is called for each element in the array. It takes the following arguments:
+element: The current element being processed.
+index (Optional): The index of the current element.
+array (Optional): The array that every() was called on.
+thisArg (Optional): An object to use as this inside the callback function.
+
+Return Value
+--------------
+true: If all elements in the array pass the test.
+false: If any element fails the test.
+*/
+//=======================================================================
+
+
+
+
+
+
+
+
+
