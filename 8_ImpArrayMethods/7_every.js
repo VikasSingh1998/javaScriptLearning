96e197171f63381be38bb53b702b44a0a12a8b85
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-08-26 18:08:45 +0530

    Update 7_every.js

--- 8_ImpArrayMethods/7_every.js
+++ 8_ImpArrayMethods/7_every.js
@@ -21,12 +21,26 @@ true: If all elements in the array pass the test.
 false: If any element fails the test.
 */
 //=======================================================================
-
-
-
-
-
-
-
-
-
+//===============================================================
+// 1. Checking if every element of the Array is Even Number
+let arr = [1, 2, 3, 4, 5, 6];
+let val = arr.every((val, index, arr) => {
+  return val % 2 === 0;
+});
+console.log(val); //false
+//------------------------------------------------------------------
+// 2. Checking if every String in an Array is Longer than 4 Characters
+let words = ["apple", "banana", "cherry", "date"];
+let val = words.some((word, index) => {
+  return word.length > 4;
+});
+console.log(val);//true
+// ------------------------------------------------------------------
+/*
+Key Points
+-----------
+Early Termination: The every() method stops executing the callback function as soon as it finds an element that does not satisfy the condition.
+Non-Mutating: every() does not change the original array.
+Works on Any Iterable: It can be used on arrays of any type of elements, including numbers, strings, objects, etc.
+The every() method is particularly useful when you need to ensure that all elements in an array meet a certain condition.
+*/
